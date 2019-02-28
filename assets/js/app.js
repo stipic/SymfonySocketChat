4b3fa15a580b772eb2971a021736a274ab891b0e
rs4 <kiki.stipic@gmail.com>
2019-02-28 13:15:48 +0100

    new socket connection port

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,7 @@ require('../css/app.css');
 var host = window.location.hostname;
 // host = '5.189.166.104';
 
-var webSocket = WS.connect("ws://" + host + ":5510");
+var webSocket = WS.connect("ws://" + host + "");
 
 webSocket.on("socket/connect", function(session) {
 
