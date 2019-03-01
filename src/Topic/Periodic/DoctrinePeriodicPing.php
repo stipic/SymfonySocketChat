7650a25fdd88df6b3d2c9c71e2a9e0591dd34992
Kristijan <kiki.stipic@gmail.com>
2019-03-01 20:03:46 +0100

    Socket period timer / DBAL ping every 30 seconds

---
+++ src/Topic/Periodic/DoctrinePeriodicPing.php
@@ -0,0 +1,86 @@
+<?php
+namespace App\Topic\Periodic;
+
+use Gos\Bundle\WebSocketBundle\Periodic\PeriodicInterface;
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\DBALException;
+use Doctrine\DBAL\Driver\PingableConnection;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+
+class DoctrinePeriodicPing implements PeriodicInterface, LoggerAwareInterface
+{
+    use LoggerAwareTrait;
+
+    /**
+     * @var Connection|PingableConnection
+     */
+    protected $connection;
+
+    /**
+     * @var int|float
+     */
+    protected $timeout = 30;
+
+    /**
+     * @param Connection|PingableConnection $connection
+     */
+    public function __construct($connection)
+    {
+        if (!($connection instanceof Connection) && !($connection instanceof PingableConnection)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    'The connection must be a subclass of %s or implement %s, %s does not fulfill these requirements.',
+                    Connection::class,
+                    PingableConnection::class,
+                    get_class($connection)
+                )
+            );
+        }
+
+        $this->connection = $connection;
+    }
+
+    /**
+     * @param int|float $timeout
+     */
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+    }
+
+    public function tick()
+    {
+        try {
+            $startTime = microtime(true);
+            $this->connection->ping();
+            $endTime = microtime(true);
+
+            if ($this->logger) {
+                $this->logger->info(
+                    sprintf('Successfully pinged database server (~%s ms)', round(($endTime - $startTime) * 100000), 2)
+                );
+            }
+        } catch (DBALException $e) {
+            if ($this->logger) {
+                $this->logger->emergency(
+                    'Could not ping database server',
+                    [
+                        'exception' => $e,
+                    ]
+                );
+                dump('Error!');
+            }
+
+            throw $e;
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+}
