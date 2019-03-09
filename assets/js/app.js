783f4d7ac2cd9e2a6d656e37345e5968c13886bd
rs4 <kiki.stipic@gmail.com>
2019-03-09 12:14:45 +0100

    removed black left nav and yarn packages added

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,8 @@ require('../css/bootstrap.min.css');
 require('../css/app.css');
 require('../css/swipe.min.css');
 
+require('popper.js');
+require('./bootstrap.min.js');
 const $ = require('jquery');
+
+$('[data-toggle="tooltip"]').tooltip();
